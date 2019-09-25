ff6b29990106eb66390e7d854156b2ebaf9e19cd
xyxz Zhao <zhaoyueke698@163.com>
2019-09-25 23:20:14 +0800

    Design layout set click listener on button

--- app/src/main/java/com/example/study_wds/MainActivity.java
+++ app/src/main/java/com/example/study_wds/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.study_wds;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button = null;
+    private boolean LEDOn = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (LEDOn) {
+                    button.setText("ALL LED OFF");
+                } else {
+                    button.setText("ALL LED ON");
+                }
+
+                LEDOn = !LEDOn;
+            }
+        });
+
     }
 }
